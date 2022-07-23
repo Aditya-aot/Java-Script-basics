94a95d2c5b119b3833d3b725deb0640c6c01612e
Aditya-aot <aditya.aot.11@gmail.com>
2022-07-23 14:06:27 +0530

    starting with javascript

---
+++ part1.js
@@ -0,0 +1,73 @@
+var a = 10 ;
+
+a= 'aditya'
+b=100
+d=88
+var c ;
+// a
+
+console.log(a,b ,c) ;
+console.log(b+b)
+
+//  math 
+console.log(Infinity) 
+console.log(Infinity-20)
+console.log(Number.MAX_VALUE) 
+console.log(1/0) 
+
+var a = null
+
+console.log(typeof(a))
+console.log(typeof("-1"))
+
+console.log(b>c)
+console.log(20 + 12 * 2 - 10 / 2)
+
+console.log(1=="1")
+console.log(1==="1")
+
+
+// Conditionals
+
+if (b > d) 
+    {console.log("b is big") ;}
+else if (b < d)
+    { console.log("d is big") ;}
+else 
+    { console.log("both are equal") ;}
+
+
+    if( -1 ) {
+        console.log("true")
+    }
+    else {
+        console.log("false")
+    }
+    
+    if( "" ) {
+        console.log("true")
+    }
+    else {
+        console.log("false")
+    }
+
+
+//  Loops
+//  while loop
+var i=1 ;
+while(i <= 5) 
+    { console.log("while loop : ",i) ;
+    i++ ;}
+
+// for loop
+var i=1 ;
+for( ; i<=5 ; i++)
+    { console.log("for loop : ",i) }
+
+// dowhile loop
+var i=1 ;
+do
+    { console.log("do while loop :",i); 
+    i++ }
+    while(i<=5) ;
+// 
