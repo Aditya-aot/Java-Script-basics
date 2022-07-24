0e575aa2a7767d277773982355b90a8a37b81f95
Aditya-aot <aditya.aot.11@gmail.com>
2022-07-24 12:14:03 +0530

    objeacts and timing events

---
+++ objects_and_timing_events.js
@@ -0,0 +1,9 @@
+
+// object
+var student = {"name":"abc", rollno:123,marks:90} ;
+console.log(student)
+console.log(student.marks)
+console.log(student["marks"])
+
+delete student.marks
+console.log(student)
