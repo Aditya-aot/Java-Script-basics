78e695ded32a225fb65bc5fe0f3c7f7e60a7bdba
Aditya-aot <aditya.aot.11@gmail.com>
2022-07-31 13:47:20 +0530

    objects and timing events

--- objects_and_timing_events.js
+++ objects_and_timing_events.js
@@ -7,3 +7,20 @@ console.log(student["marks"])
 
 delete student.marks
 console.log(student)
+
+
+// 
+
+for (var prop in student) {
+    console.log(prop, student[prop]) ;
+}
+
+// or
+var keys = Object.keys(student) ;
+console.log(keys)
+
+// timing events
+function sayhello() {
+    console.log("hello") ;
+}
+setTimeout(sayhello, 5000) ;
