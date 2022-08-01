de4f00395d4fc4a6ac07e432ee7b4bb9f6dc0c24
Aditya-aot <aditya.aot.11@gmail.com>
2022-08-01 23:00:26 +0530

    objects and timing events

--- objects_and_timing_events.js
+++ objects_and_timing_events.js
@@ -1,29 +1,29 @@
 
-// // object
-// var student = {"name":"abc", rollno:123,marks:90} ;
-// console.log(student)
-// console.log(student.marks)
-// console.log(student["marks"])
+// object
+var student = {"name":"abc", rollno:123,marks:90} ;
+console.log(student)
+console.log(student.marks)
+console.log(student["marks"])
 
-// delete student.marks
-// console.log(student)
+delete student.marks
+console.log(student)
 
 
-// // 
+// 
 
-// for (var prop in student) {
-//     console.log(prop, student[prop]) ;
-// }
+for (var prop in student) {
+    console.log(prop, student[prop]) ;
+}
 
-// // or
-// var keys = Object.keys(student) ;
-// console.log(keys)
+// or
+var keys = Object.keys(student) ;
+console.log(keys)
 
-// // timing events
-// function sayhello() {
-//     console.log("hello") ;
-// }
-// setTimeout(sayhello, 2000) ;
+// timing events
+function sayhello() {
+    console.log("hello") ;
+}
+setTimeout(sayhello, 2000) ;
 
 
 // 
