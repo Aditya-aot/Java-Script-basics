cdd5a7c9a3bf19e95a0a370d95c35a2b56117efe
Aditya-aot <aditya.aot.11@gmail.com>
2022-08-01 23:01:35 +0530

    objects and timing events

--- objects_and_timing_events.js
+++ objects_and_timing_events.js
@@ -27,10 +27,15 @@ setTimeout(sayhello, 2000) ;
 
 
 // 
-var d = {h: 2, j: 3}
-
-var e = d
-var f = {...d, j: 4}
-
-console.log(f.h)
-console.log(f.j)
+function change1(obj) {
+    obj.name = "ninjas"
+  }
+  function change2(obj) {
+    obj = {name: "changed"}
+  }
+  
+  var obj = {name:"coding"}
+  
+  change1(obj)
+  change2(obj)
+  console.log(obj.name)  
