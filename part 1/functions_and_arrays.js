1a8bac01a943777a7a167a13b4a31b2640f9f868
Aditya-aot <aditya.aot.11@gmail.com>
2022-08-24 12:08:17 +0530

    backend base

--- part 1/functions_and_arrays.js
+++ part 1/functions_and_arrays.js
@@ -1,81 +1,91 @@
-function showAlert(msg) {
-    console.log(msg) ;
+// function showAlert(msg) {
+//     console.log(msg) ;
+// }
+// showAlert("hey !!") ;
+
+// // Hoisting 
+// function hoistDemo() {
+//     console.log(i) ;
+//     var i =10 ;
+// }
+// hoistDemo() ;
+
+// // Function Expressions
+// var factorial = function fact(n) {
+//     var ans = 1;
+//     for(var i = 1; i<= n ; i++){
+//         ans = ans* i ;
+//     }
+//     return ans ;
+// } ;
+
+// console.log(factorial(5)) ;
+
+// // 
+// var add = function(a, b) {
+//     return a+b;
+// }
+
+// var subtract = function(a, b) {
+//     return a-b;
+// }
+
+// var op = function (func) {
+//     var x = 2;
+//     var y = 3;
+//     return func(x, y);
+// }
+
+// console.log(op(add));
+
+// // Array
+// var arr1= [1,2,3,4,5] ;
+// var arr2 = new Array(6,7,8,9,10) ; 
+// var arr3 = new Array(1000)
+
+// console.log(arr1,arr2 , arr3);
+// console.log(arr1[3]);
+
+// arr1[8] = 100;
+// console.log(arr1);
+
+// // Splice Array
+// var fruits = ['Apple', 'Orange', 'Kiwi', 'Strawberry','22'];
+
+// fruits.splice(fruits.length, 1, fruits[0]);
+// fruits.splice(0,1)
+// // fruits.splice(4, 1, fruits[0]);
+// console.log(fruits);
+
+
+// // 
+// function f(a,b = 1) {
+//     console.log(a*b)
+//   }
+//   var x = // some hidden  value
+//   f(5,x)
+
+
+// // 
+
+
+function cart() {
+    let items = 0;
+    return {
+       addItem: function () {
+            items++;
+        },
+        getItem: function () {
+            return items;
+        }
+    };
 }
-showAlert("hey !!") ;
 
-// Hoisting 
-function hoistDemo() {
-    console.log(i) ;
-    var i =10 ;
-}
-hoistDemo() ;
-
-// Function Expressions
-var factorial = function fact(n) {
-    var ans = 1;
-    for(var i = 1; i<= n ; i++){
-        ans = ans* i ;
-    }
-    return ans ;
-} ;
-
-console.log(factorial(5)) ;
-
-// 
-var add = function(a, b) {
-    return a+b;
-}
-
-var subtract = function(a, b) {
-    return a-b;
-}
-
-var op = function (func) {
-    var x = 2;
-    var y = 3;
-    return func(x, y);
-}
-
-console.log(op(add));
-
-// Array
-var arr1= [1,2,3,4,5] ;
-var arr2 = new Array(6,7,8,9,10) ; 
-var arr3 = new Array(1000)
-
-console.log(arr1,arr2 , arr3);
-console.log(arr1[3]);
-
-arr1[8] = 100;
-console.log(arr1);
-
-// Splice Array
-var fruits = ['Apple', 'Orange', 'Kiwi', 'Strawberry','22'];
-
-fruits.splice(fruits.length, 1, fruits[0]);
-fruits.splice(0,1)
-// fruits.splice(4, 1, fruits[0]);
-console.log(fruits);
-
-
-// 
-function f(a,b = 1) {
-    console.log(a*b)
-  }
-  var x = // some hidden  value
-  f(5,x)
-
-
-
-
-
-
-
-
-
-
-
-
+const closure = cart();
+closure.addItem();
+closure.addItem();
+closure.addItem();
+console.log(closure.getItem());
 
 
 
